9ac532c2a0bfcc88fe6a61a3d8e9f10ed5791cd4
Dylan Rogers <opendylan@proton.me>
2025-09-11 16:00:45 +0100

    c1e3

---
+++ c1e3/Program.cs
@@ -0,0 +1,9 @@
+﻿namespace Program {
+    public class Program {
+	public static void Main(String[] args) {
+	    Console.Write("Hello World!");
+	    Console.WriteLine(" This is text on the same line...");
+	    Console.WriteLine("And this is text on a new line!");
+	}
+    }
+}
