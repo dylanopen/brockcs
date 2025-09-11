db13f07ac90e0c07acea566e6b00d04ec3abdf40
Dylan Rogers <opendylan@proton.me>
2025-09-11 16:03:53 +0100

    c1e5

---
+++ c1e5/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace Program {
+    public class Program {
+	public static void Main(String[] args) {
+	   String rocket = """
+      ***
+     *****
+    *******
+    *******
+    *******
+    *******
+    *******
+    *******
+    *******
+    *******
+      ***
+     *****
+""";
+
+	   for (int i=8; i>0; i--) {
+		System.Threading.Thread.Sleep(1000);
+		Console.Clear();
+		for (int j=0; j<i; j++)
+		   Console.WriteLine();
+		Console.WriteLine(rocket);
+	   }
+	}
+    }
+}
