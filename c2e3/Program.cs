17ea805c2ccc36fb62778bd126000edadf494bfb
Dylan Rogers <opendylan@proton.me>
2025-09-17 14:06:52 +0100

    c2e3

---
+++ c2e3/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Write("Width = ");
+double width = Convert.ToDouble(Console.ReadLine());
+Console.Write("Length = ");
+double length = Convert.ToDouble(Console.ReadLine());
+Console.Write("Depth = ");
+double depth = Convert.ToDouble(Console.ReadLine());
+
+double volume = width*length*depth;
+
+Console.WriteLine("Volume = " + volume);
