574f59556014c013ad9978d7deed8e54157e2257
Dylan Rogers <opendylan@proton.me>
2025-09-17 14:18:47 +0100

    c3e2

---
+++ c3e2/Program.cs
@@ -0,0 +1,6 @@
+﻿Console.Write("Enter first number: ");
+double a = Convert.ToDouble(Console.ReadLine());
+Console.Write("Enter second number: ");
+double b = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine($"{a}*{b}={a*b}");
+
