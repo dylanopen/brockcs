aa9e4608dfe7ca7d63d63e05261f530004284afb
Dylan Rogers <opendylan@proton.me>
2025-09-17 14:06:27 +0100

    c2e2

---
+++ c2e2/Program.cs
@@ -0,0 +1,12 @@
+﻿// As it was not specified, there is no error handling in this program and it
+// will simply crash if the input is not representable by a float.
+
+Console.Write("Enter first number: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Enter second number: ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+
+int product = num1 * num2;
+
+Console.WriteLine("Product of numbers: " + product);
+
