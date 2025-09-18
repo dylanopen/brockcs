2e8300f8487e6a6dafe61d67f0b9664349157a0b
Dylan Rogers <opendylan@proton.me>
2025-09-18 15:31:24 +0100

    c3e7

---
+++ c3e7/Program.cs
@@ -0,0 +1,15 @@
+﻿const double BORDER = 0.5;
+const double COST_PER_SQUARE_METRE = 0.75;
+
+Console.Write("Width (m) = ");
+double width = Convert.ToDouble(Console.ReadLine());
+Console.Write("Height (m) = ");
+double height = Convert.ToDouble(Console.ReadLine());
+
+width -= BORDER*2;
+height -= BORDER*2;
+
+double cost = width * height * COST_PER_SQUARE_METRE;
+
+Console.WriteLine($"Cost: {cost:N2}");
+
