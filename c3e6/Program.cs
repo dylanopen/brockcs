3889b50113c15a8a57e0c5a9f748b3932d0a097c
Dylan Rogers <opendylan@proton.me>
2025-09-18 15:21:20 +0100

    c3e6

---
+++ c3e6/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Height (cm) = ");
+double cms = Convert.ToDouble(Console.ReadLine());
+Console.Write("Weight (kg) = ");
+double kgs = Convert.ToDouble(Console.ReadLine());
+
+double ins = cms * 2.54;
+double lbs = kgs / 2.2;
+
+Console.WriteLine($"Height (ins) = {ins:N2}");
+Console.WriteLine($"Weight (lbs) = {lbs:N2}");
+
