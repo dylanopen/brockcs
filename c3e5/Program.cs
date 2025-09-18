3405a1d90c80ec150b678e6d67bf0cd912356627
Dylan Rogers <opendylan@proton.me>
2025-09-18 15:15:21 +0100

    c3e5

---
+++ c3e5/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Gross Price: £");
+double gross = Convert.ToDouble(Console.ReadLine());
+Console.Write("Discount rate (%): ");
+double discountPercentage = Convert.ToDouble(Console.ReadLine()) * 0.01;
+
+double discount = gross * discountPercentage;
+double finalPrice = gross - discount;
+
+Console.WriteLine($"Discount: {discount:C}");
+Console.WriteLine($"Discount price: {finalPrice:C}");
+
