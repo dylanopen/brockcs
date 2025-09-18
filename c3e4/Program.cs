9d977550bde86955736a91771556515903bce103
Dylan Rogers <opendylan@proton.me>
2025-09-18 15:06:38 +0100

    c3e4

---
+++ c3e4/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.WriteLine("Enter 5 examination marks:");
+double total = 0.0;
+for (int i = 0; i < 5; i++) {
+    total += Convert.ToInt32(Console.ReadLine());
+}
+double mean = total / 5.0;
+Console.WriteLine("Total: " + total);
+Console.WriteLine("Mean: " + mean);
