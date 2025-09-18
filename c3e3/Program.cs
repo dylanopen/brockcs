19281e587e8b1c4d0e678a1a9a312f4ab2611b09
Dylan Rogers <opendylan@proton.me>
2025-09-18 15:06:34 +0100

    c3e3

---
+++ c3e3/Program.cs
@@ -0,0 +1,5 @@
+﻿Console.Write("Temperature in Fahrenheit: ");
+double fahrenheit = Convert.ToDouble(Console.ReadLine());
+double celcius = (fahrenheit - 32.0) * (5.0/9.0);
+Console.WriteLine("Temperature in Celcius: " + celcius);
+
